da1c4b1aa7d10c0aec0970df98a84789c40ca52f
Marc Pomar <marc@faable.com>
2020-09-02 14:00:02 +0200

    done

---
+++ solutions/test-import/app.py
@@ -0,0 +1,8 @@
+
+
+
+def app():
+    print("Hola App",__name__)
+
+if __name__ == "__main__":
+    app()
---
+++ solutions/test-import/main.py
@@ -0,0 +1,8 @@
+
+from app import app
+
+def main():
+    print("Hola Main",__name__)
+    app()
+
+main()
