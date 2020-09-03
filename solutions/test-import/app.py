2ae97ecf0a892d633568021f023dd3f0ae2a7a46
Marc Pomar <marc@faable.com>
2020-09-03 13:51:51 +0200

    done

--- solutions/test-import/app.py
+++ solutions/test-import/app.py
@@ -1,8 +1,9 @@
-
-
+print("Primera importacion")
+from saluda import edad
 
 def app():
     print("Hola App",__name__)
+    #saludaNombre()
 
 if __name__ == "__main__":
     app()
---
+++ solutions/test-import/saluda.py
@@ -0,0 +1,11 @@
+
+print("🔥")
+def saludaNombre(name="Pepe"):
+    print(f"Hola, eres {name}")
+
+def edad(x="Pepe"):
+    print(f"Tienes {x} años")
+
+print("📦")
+if __name__ == "__main__":
+    print("📦📦📦📦📦")
