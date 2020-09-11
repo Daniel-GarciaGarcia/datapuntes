99c5e8857510a90490e050a387c29b338b6e4a02
Marc Pomar <marc@faable.com>
2020-09-11 12:29:42 +0200

    done

--- w4-d5-argparse/main.py
+++ w4-d5-argparse/main.py
@@ -1,10 +1,42 @@
 #!/usr/bin/env python3
 import sys
+import argparse
+
+# https://docs.python.org/3/library/argparse.html
+
+
+def onlyYears(yearStr, minYear=1980, maxYear=2010):
+    year = 2020
+    try:
+       year = int(yearStr)
+    except Exception:
+         raise argparse.ArgumentTypeError(f"{yearStr} is an invalid positive int value")
+     
+    if year > minYear and year < maxYear:
+        return year
+    else:
+        raise argparse.ArgumentTypeError(f"year must be between {minYear} and {maxYear}")
+
 
 
 def main():
-    print("Hola")
-    print(sys.argv)
+    parser = argparse.ArgumentParser(description='Saluda a un vecino')
+    parser.add_argument('-a', dest='apellidos',
+                        default="Garcia",
+                        help='los apellidos de la persona a la que saludar')
+    parser.add_argument('-n', dest='nombre',
+                        default="Pepe",
+                        help='el nombre de la persona a la que saludar')
+    parser.add_argument('-y', dest='year',
+                        default=2020,
+                        type=onlyYears,
+                        help="Año de nacimiento")
+                        
+    args = parser.parse_args()
+    #print(args)
+    nombre = args.nombre
+    apellidos = args.apellidos
+    print(f"Hola que tal estas {nombre} {apellidos} naciste el {args.year}")
 
 if __name__ == "__main__":
     main()
